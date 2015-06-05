8d2cba5e02a8312482b9c96960ea028aaa19d0d3
Nicolas Perez <nicoperez444@gmail.com>
2015-06-05 17:22:58 -0300

    Import Inicial

---
+++ QrWebApp/QrWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QrWebApp.Startup))]
+namespace QrWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
