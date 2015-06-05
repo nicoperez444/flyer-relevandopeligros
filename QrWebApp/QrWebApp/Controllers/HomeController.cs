16dcd86e8b2193ed95cb60504217d1a455531eee
Alejandro Guzman <alejocba04@hotmail.com>
2015-06-05 19:52:52 -0300

    Commiteo la imagen qr

--- QrWebApp/QrWebApp/Controllers/HomeController.cs
+++ QrWebApp/QrWebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using com.libraries.qrgenerator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,8 @@ namespace QrWebApp.Controllers
     {
         public ActionResult Index()
         {
+            GeneradorQr qr = new GeneradorQr();
+
             return View();
         }
 
---
+++ QrWebApp/com.libraries.qrgenerator/GeneradorQr.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows;
+using System.Drawing.Imaging;
+using System.Drawing;
+using Gma.QrCodeNet.Encoding.Windows.Controls;
+
+namespace com.libraries.qrgenerator
+{
+    public class GeneradorQr
+    {
+        public GeneradorQr()
+        {
+            GenerarQr("D:/", "http://relevandopeligros.com/Peligro/InfoPeligro/282", 50);
+        }
+
+        private void GenerarQr(string dir, string content, int image_size)
+        {
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            QrCode qrCode = qrEncoder.Encode(content);
+
+            Renderer renderer = new Renderer(image_size, Brushes.Black, Brushes.White);
+
+            MemoryStream ms = new MemoryStream();
+
+            renderer.WriteToStream(qrCode.Matrix, ms, ImageFormat.Png);
+
+            //renderer.WriteToStream(qrCode.Matrix, ms, ImageFormat.Png);
+
+            var imageTemp = new Bitmap(ms);
+
+            var image = new Bitmap(imageTemp, new Size(new Point(200, 200)));
+
+            image.Save(dir + "qr.png", ImageFormat.Png);
+        }
+    }
+
+}
