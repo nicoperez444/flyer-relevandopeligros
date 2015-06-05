12dc62c8555098bee33fb5897c00aea2b387e31b
Nicolas Perez <nicoperez444@gmail.com>
2015-06-05 19:50:52 -0300

    Cambio del index

--- QrWebApp/QrWebApp/Controllers/HomeController.cs
+++ QrWebApp/QrWebApp/Controllers/HomeController.cs
@@ -13,6 +13,12 @@ namespace QrWebApp.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(String url)
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
