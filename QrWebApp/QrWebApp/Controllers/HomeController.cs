252d64b2ec4feedf08bca1b0c070ca3f5cd97d47
Alejandro Guzman <alejocba04@hotmail.com>
2015-06-06 11:48:47 -0300

    Nuevo llamado al qr

--- QrWebApp/QrWebApp/Controllers/HomeController.cs
+++ QrWebApp/QrWebApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using com.libraries.qrgenerator;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,21 @@ namespace QrWebApp.Controllers
         public ActionResult Index()
         {
             QrGenerator qr = new QrGenerator();
+
+            //MODIFICAR LA URL NOMAS
+            String url = "http://relevandopeligros.com/Peligro/InfoPeligro/282";
+
+
+
+
+
+            String id = "QR_" + url.Replace("http://relevandopeligros.com/Peligro/InfoPeligro/", "");
+
+            //CREA EL QR CON LOCO
+            Bitmap bitmap = qr.GetQr(url, 500, 100);
+
+            //GUARDO EL QR
+            qr.SaveQr("C:/", id, bitmap);
 
             return View();
         }
